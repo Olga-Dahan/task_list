e4773d512afaa225861dfb13da81f6ebf5e38926
Olga-Dahan <liordior92@gmail.com>
2023-08-23 22:38:43 +0300

    initial

---
+++ assets/js/task.js
@@ -0,0 +1,53 @@
+var taskList = [];  
+
+
+function loadTask() {
+
+    var task = document.getElementById("the-task").value;      
+    var ifDone = document.getElementById("if-Done").value;     
+    var date = document.getElementById("the-date").value;   
+
+    
+    var data = new Object();
+    data.task = task;
+    data.ifDone = ifDone;
+    data.date = date;
+
+    taskList.push(data);
+
+    createTable();
+    
+    document.getElementById("taskForm").reset();
+
+}
+
+function createTable() {
+
+    var data = document.getElementById("data");                     
+
+    
+    var info = "";
+
+    for (var index = 0; index < taskList.length; index++) {
+        info += `
+        <tr>
+            <td>${taskList[index].date}</td>
+            <td>${taskList[index].ifDone}</td>
+            <td>${taskList[index].task}</td>
+        </tr>
+    `
+    }
+
+    data.innerHTML = info;
+}
+
+function clearTable() {
+    document.getElementById("data").innerHTML = "";
+    taskList = [];
+}
+
+function removeLast() {
+    taskList.pop();
+    createTable();
+}
+
